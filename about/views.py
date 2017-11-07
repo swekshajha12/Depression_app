73fdcab0600f5fb79834e20a78b026343c34a0a1
sweksha12 <jhasweksha12@gmail.com>
2017-11-07 10:24:20 +0530

    added a new view

--- about/views.py
+++ about/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the college club homepage</h1>")
+def project(request):
+    return HttpResponse("<h2>The project is a sentimental analysis of depression rates over various countries</h2>"
+                        "<p> It uses the following</p>"
+                        "<li>D3 : <href>https://d3js.org/</href></li>"
+                        "<li>Twitter Stream API : <href>https://www.npmjs.com/package/twitter-stream-api</href></li>"
+                        "<li>Django 1.11 :<href>https://docs.djangoproject.com/en/1.11/</href></li>"
+                        "<li>Sentimental analysis:<href></li>")
