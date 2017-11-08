c2268534cd9b9548a23e83023739d8acb9ce2be9
sweksha12 <jhasweksha12@gmail.com>
2017-11-08 21:07:21 +0530

    home page

--- about/views.py
+++ about/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("<h1>This is the college club homepage</h1>")
+    template = loader.get_template("about/index.html")
+    #return HttpResponse("<h1>This is the college club homepage</h1>")
+
 def project(request):
     return HttpResponse("<h2>The project is a sentimental analysis of depression rates over various countries</h2>"
                         "<p> It uses the following</p>"
